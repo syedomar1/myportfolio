7dee17e43947c2c9e960defeef84593f555cb0ab
Syed Omar Albeez <syedomar.albeez2022@vitstudent.ac.in>
2024-11-01 12:30:08 +0530

    Updated Experience page

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -11,22 +11,22 @@ import {FaBriefcase} from 'react-icons/fa';
 
 function Experience() {
   return (
-    <section className='section' id="experience">
-      <div className="container mx-auto mt-0">
+    <section className='section md:-mt-[150px]' id="experience">
+      <div className="container mx-auto md:mt-[850px]">
         <div className='flex flex-col items-center justify-center'>
         <motion.div 
           variants={fadeIn('right',0.3)}
           initial='hidden'
           whileInView={"show"}
           viewport={{once:false, amount:0.3}}
-          className='lg:bg-bottom mt-64 mb-12 lg:mb-0 text-center'>
+          className='lg:bg-bottom text-center'>
             <h2 className='h2 text-accent mb-6 mt-28'>My Experience</h2>
           </motion.div>
       <VerticalTimeline lineColor="#3e497a">
         <VerticalTimelineElement
           className="vertical-timeline-element--education text-black"
           dateClassName="text-white"
-          date="Nov 2023 - Present"
+          date="June 2024 - July 2024"
           iconStyle={{ background: "#3e497a", color: "#fff" }}
           contentStyle={{
             backgroundColor: "rgb(151 112 173)",
@@ -37,28 +37,48 @@ function Experience() {
           icon={<FaBriefcase />}
         >
           <h3 className="vertical-timeline-element-title" >
-            Beta Microsoft Student Ambassador - Microsoft
+            Full Stack Developer Intern - Ethnus
           </h3>
-          <p>I learn, improve my skills, conduct workshops and contribute to the community</p>
+          <p>Developed and deployed MERN stack application, optimizing efficiency and reducing loading time by 10%.  Collaborated with cross-functional team to design and implement a Query-Helpdesk platform, enabling users to raise tickets
+          and get them resolved.</p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--education text-black"
           dateClassName="text-white"
-          date="Aug 2023 - Present"
+          date="Sept 2024 - Present"
           iconStyle={{ background: "#3e497a", color: "#fff" }}
           contentStyle={{
             backgroundColor: "rgb(151 112 173)",
           }}
           contentArrowStyle={{
-                borderRightColor: "white",
-              }}
+            borderRightColor: "white",
+          }}
           icon={<FaBriefcase />}
         >
           <h3 className="vertical-timeline-element-title">
-            Management Lead - Google Developer Student Clubs
+            Secretary - Google Developer Groups VITC
           </h3>
-          <p>With over a year experience being in the Management department, I have organized many events with collaborative efforts by the team. Also conducted Recruitment drive for the the Club. Organizing one of Tamil Nudu's biggest hackathon <a target='_blank' rel="noopener noreferrer" href='https://www.devshouse.tech/' style={{ color: 'blue'}}>"DevsHouse"</a></p>
+          <p>Oversee departmental functions and act as an advisory. Served as Management Lead for over a year and organized
+          multiple events and workshops.Organized one of Tamil Nudu's largest hackathon <a target='_blank' rel="noopener noreferrer" href='https://www.devshouse.in/' style={{ color: 'blue'}}>"DevsHouse"</a></p>
         </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--education text-black"
+            dateClassName="text-white"
+            date="Nov 2023 - Present"
+            iconStyle={{ background: "#3e497a", color: "#fff" }}
+            contentStyle={{
+              backgroundColor: "rgb(151 112 173)",
+            }}
+            contentArrowStyle={{
+                  borderRightColor: "white",
+                }}
+            icon={<FaBriefcase />}
+          >
+            <h3 className="vertical-timeline-element-title" >
+              Beta Microsoft Student Ambassador - Microsoft
+            </h3>
+            <p>I learn, improve my skills, conduct workshops and contribute to the community</p>
+          </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--work text-black"
           dateClassName="text-white"
@@ -76,6 +96,24 @@ function Experience() {
             Web Developer - Microsoft Innovations Club
           </h3>
           <p>Hosted and organized large-scale tech events as a Management Member, and now currently devoloping end to end products.</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work text-black"
+          dateClassName="text-white"
+          date=" June 2024 - Present"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          contentStyle={{
+            backgroundColor: "rgb(151 112 173)",
+          }}
+          contentArrowStyle={{
+                borderRightColor: "white",
+              }}
+          icon={<FaBriefcase />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Technical Member - CloudOps Club
+          </h3>
+          <p>Redesigned the club’s website, increasing user engagement by 25%.</p>
         </VerticalTimelineElement>
 
         <VerticalTimelineElement
@@ -95,7 +133,7 @@ function Experience() {
             Founder President - Creative Gavel Club, Toastmasters Intl'
           </h3>
           <p>
-            Became the Competent Communicator, won many Intl' and Humurous Speech contest in Area Level, Conducted contests, welcomed new members into the Toastmaster fraternity.
+            Became the Competent Communicator, won several Intl' and Humurous Speech contest in Area Level, Conducted contests, welcomed new members into the Toastmaster fraternity.
           </p>
         </VerticalTimelineElement>
       </VerticalTimeline>
